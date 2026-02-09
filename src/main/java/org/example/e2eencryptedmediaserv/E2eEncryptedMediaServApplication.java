f7ad2bca5b7053dda6153fd50620a0c2f9197a15
Rembqq <nikitavolkovskyi@gmail.com>
2026-02-09 22:06:19 +0100

    initial structure

---
+++ src/main/java/org/example/e2eencryptedmediaserv/E2eEncryptedMediaServApplication.java
@@ -0,0 +1,13 @@
+package org.example.e2eencryptedmediaserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eEncryptedMediaServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2eEncryptedMediaServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/e2eencryptedmediaserv/E2eEncryptedMediaServApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.e2eencryptedmediaserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eEncryptedMediaServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
