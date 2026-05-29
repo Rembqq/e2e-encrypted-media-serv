33a2ffe92dc373a72a4384c2c8bfc3b1ddff0f94
Rembqq <nikitavolkovskyi@gmail.com>
2026-05-29 19:55:38 +0200

    redeploy2

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL)
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://e2e-encrypted-media-serv.onrender.com/api/v1'
 })
